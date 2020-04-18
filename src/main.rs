153c141e7823c0511e36c66b659dd4dd30d651be
Alexey Kramarenko <kramar42@gmail.com>
2020-04-18 23:56:09 +0200

    Sierpinski

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use nannou::app::LoopMode;
 mod r201;
 use r201::R201;
 
+const POINT_SIZE: f32 = 1.;
+const TRIANGLE_SIZE: f64 = 250.;
+const MIN_SIZE: f64 = 0.7;
+
 fn point(x: f64, y: f64) -> R201 {
     R201::e12() - x * R201::e02() + y * R201::e01()
 }
@@ -79,6 +83,7 @@ fn main() {
 }
 
 struct Model {
+    x: R201,
     a: R201,
     o: R201,
     m: R201,
@@ -87,10 +92,11 @@ struct Model {
 
 fn model(_app: &App) -> Model {
     let o = point(0., 0.);
-    let x = point(1., 1.);
+    let x = point(0., -100.);
     let mut res = Model {
-        a: x,
-        o: o,
+        x: x,
+        a: o,
+        o: point(0., 0.),
         l: R201::zero(),
         m: R201::zero(),
     };
@@ -118,17 +124,40 @@ fn draw_axis(draw: &Draw) {
         .color(BLACK);
 }
 
+fn triangle(draw: &Draw, x: &R201, y: f64) {
+    if y < MIN_SIZE {
+        draw_p(draw, &x, "1");
+        return;
+    }
+
+    let angle = PI/3.;
+    let dy = angle.sin() as f64 * y * R201::e01();
+    let dx = -y * R201::e02();
+    let p1 = x + &dy;
+    let p2 = x - &dy * angle.cos() as f64 + &dx;
+    let p3 = x - &dy * angle.cos() as f64 - &dx;
+    //* sin(PI/3) - y * R201::e02();
+    triangle(draw, &p1, y / 2.);
+    triangle(draw, &p2, y / 2.);
+    triangle(draw, &p3, y / 2.);
+}
+
 fn view(app: &App, model: &Model, frame: Frame) {
-    app.set_loop_mode(LoopMode::wait());
+    app.set_loop_mode(LoopMode::loop_once());
+    //app.set_loop_mode(LoopMode::wait());
     let draw = app.draw();
 
-    draw.background().color(Rgba::new(0.7, 0.7, 0.7, 0.7));
-    draw_axis(&draw);
+    draw.background().color(BLACK);
+        //Rgba::new(0.7, 0.7, 0.7, 0.7));
+    //draw_axis(&draw);
 
-    draw_l(&draw, &model.l);
-    draw_p(&draw, &model.a, "A");
-    draw_p(&draw, &model.o, "0");
-    draw_p(&draw, &model.m, "M");
+    //draw_p(&draw, &model.x, "o");
+    triangle(&draw, &model.x, TRIANGLE_SIZE);
+
+    //draw_l(&draw, &model.l);
+    //draw_p(&draw, &model.a, "A");
+    //draw_p(&draw, &model.o, "0");
+    //draw_p(&draw, &model.m, "M");
 
     draw.to_frame(app, &frame).unwrap();
 }
@@ -150,13 +179,13 @@ fn event(_app: &App, model: &mut Model, event: Event) {
 fn draw_p(draw: &Draw, p: &R201, t: &str) {
     let (x, y) = p.to_xy();
     draw.ellipse()
-        .radius(5.0)
+        .radius(POINT_SIZE)
         .color(RED)
         .x_y(x as f32, y as f32);
-    draw.text(t)
-        .color(BLACK)
-        .up(5.)
-        .left(5.);
+    //draw.text(t)
+        //.color(BLACK)
+        //.up(5.)
+        //.left(5.);
 }
 
 fn draw_l(draw: &Draw, l: &R201) {
