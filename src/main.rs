726508249131bcc8b815111d71c2d4bd3f763c20
Alexey Kramarenko <kramar42@gmail.com>
2020-04-18 02:31:36 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,208 @@
+//#![windows_subsystem = "windows"]
+#[link(name = "vcruntime")]
+
+/// в центре статическая картинка
+/// передвижение мышки по четырем углам окна передвигает нас по плоскости параметров
+/// зажатие и движение меняет обзорный вектор
+///
+
+use nannou::prelude::*;
+use nannou::draw::Draw;
+//use nannou::app::LoopMode;
+
+mod r201;
+use r201::R201;
+
+fn point(x: f64, y: f64) -> R201 {
+    R201::e12() - x * R201::e02() + y * R201::e01()
+}
+
+fn ipoint(x: f64, y: f64) -> R201 {
+    -x * R201::e02() + y * R201::e01()
+}
+
+fn line(a: f64, b: f64, c: f64) -> R201 {
+    a * R201::e1() + b * R201::e2() + c * R201::e0()
+}
+
+impl R201 {
+    pub fn scale(self: &Self, s: f64) -> R201 {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a[0] * s;
+        res[1] = a[1] * s;
+        res[2] = a[2] * s;
+        res[3] = a[3] * s;
+        res[4] = a[4] * s;
+        res[5] = a[5] * s;
+        res[6] = a[6] * s;
+        res[7] = a[7] * s;
+        res
+    }
+
+    pub fn length(self: &Self) -> f64 {
+        //(self * !self).abs().sqrt()
+        1.0
+    }
+
+    pub fn to_xy(self: &Self) -> (f64, f64) {
+        (-self[5] * 50., self[4] * 50.)
+    }
+
+    pub fn to_center(self: &Self) -> (f64, f64) {
+        (-self[2] * 50., self[1] * 50.)
+    }
+}
+
+/*
+impl Iter {
+    pub fn partition(self: &Self, n: usize) -> (self.Item, self.Item) {
+    }
+}
+*/
+
+fn coeff(_coefs: &[f64]) -> R201 {
+    let res = R201::zero();
+    //for (i, c) in coefs.iter().partition(2) {
+        //res[i] = c;
+    //}
+    res
+}
+
+fn pr(p: &R201) {
+    println!("{:}", p);
+}
+
+fn main() {
+    let p = ipoint(0.0, 0.0);
+    pr(&p);
+    let a = point(-1.0, 1.0);
+    let b = point(1.0, 1.0);
+    //let c = (&a + &b).normalized();
+    let l = &a & &b;
+
+    let x = line(0.0, 1.0, 0.0);
+    let y = line(1.0, 0.0, -10.0);
+
+    pr(&l);
+    //(&x, &y) = l.to_center();
+    println!("x: {}, y: {}", x, y);
+
+    /*
+    let no = coeff(0.5, -0.5); // some coeff
+    let ni = coeff(1.0, -1.0);
+
+    let ni_part = &p | (no.scale(-1.0)); // O_i + n_o O_oi
+    let no_part = ni.scale(-1.0) | &p;   // O_o + O_oi n_i
+
+    let no_ni_part = &no_part | no.scale(-1.0); // O_oi
+    let no_only_part = ni ^ no_part | no.scale(-1.0); // O_oi
+
+    let direction = no_ni_part;
+    //let p = p.scale(1 / dl);
+    */
+
+    //let intersect = (&l ^ &y).normalized();
+    //println!("{:?}", intersect.to_xy());
+
+    /*
+    let minus_no = no.Scale -1;
+
+    let directior = no_ni_part;
+    let dl = direction.length;
+    o = o.scale(1/dl);
+    let lx = o.e1;
+    let ly = -o.e2;
+    */
+
+    nannou::app(model)
+        //.update(update)
+        //.set_loop_mode(LoopMode::refresh_sync())
+        //.event(event)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn _update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn draw_axis(draw: &Draw) {
+    // improve: draw in geom terms
+    draw.rect()
+        .w(2000.)
+        .h(2.)
+        .color(BLACK);
+    draw.rect()
+        .w(2.)
+        .h(2000.)
+        .color(BLACK);
+}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let a = point(-3., 0.);
+    let b = point( 3., 2.);
+    let c = (&a + &b).normalized();
+    let l = &b & &a;
+
+    let draw = app.draw();
+    draw.background().color(BLUE);
+    draw_p(&draw, &a);
+    draw_p(&draw, &b);
+    draw_p(&draw, &c);
+    draw_l(&draw, &l);
+    draw_axis(&draw);
+
+    //frame.clear(PURPLE);
+    draw.to_frame(app, &frame).unwrap();
+}
+
+fn _event(_app: &App, _model: &mut Model, event: Event) {
+    println!("got event: {:?}", event);
+}
+
+fn draw_p(draw: &Draw, p: &R201) {
+    //pr(&p);
+
+    let (x, y) = p.to_xy();
+
+    draw.ellipse()
+        .radius(5.0)
+        .color(RED)
+        .x_y(x as f32, y as f32);
+}
+
+fn draw_l(draw: &Draw, l: &R201) {
+    //pr(l);
+    //let no = ipoint(0.5, 0.5);
+    //let minus_no = no.scale(-1.);
+    //let loc = &minus_no | l;
+    //pr(&l);
+    let (x, y) = l.normalized().to_center();
+    //println!("x: {}, y: {}", x, y);
+    let start_point = pt2(-100.0 + x as f32, y as f32);
+    let end_point   = pt2(100.0 + x as f32, y as f32);
+    //println!("start: {:?}, end: {:?}", start_point, end_point);
+
+    let xl = line(0., -1., 0.);
+    let lr = (l.normalized() | xl.normalized())[0].acos() as f32;
+    //println!("angle: {}", lr);
+
+    draw.rect()
+        //.weight(2.0)
+        .w(2000.)
+        .h(4.)
+        .color(GREEN)
+        .x_y(x as f32, y as f32)
+        //.start(start_point)
+        //.end(end_point);
+        .rotate(lr);
+}
+
+fn _sketch(app: &App, _frame: Frame) {
+}
---
+++ src/r201.rs
@@ -0,0 +1,576 @@
+// 3D Projective Geometric Algebra
+// Written by a generator written by enki.
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+
+use std::fmt;
+use std::ops::{Index,IndexMut,Add,Sub,Mul,BitAnd,BitOr,BitXor,Not};
+
+type float_t = f64;
+
+// use std::f64::consts::PI;
+const PI: float_t = 3.14159265358979323846;
+
+const basis: &'static [&'static str] = &[ "1","e0","e1","e2","e01","e02","e12","e012" ];
+const basis_count: usize = basis.len();
+
+#[derive(Default,Debug,Clone,PartialEq)]
+pub struct R201 {
+    mvec: Vec<float_t>
+}
+
+impl R201 {
+    pub fn zero() -> Self {
+        Self {
+            mvec: vec![0.0; basis_count]
+        }
+    }
+
+    pub fn new(f: float_t, idx: usize) -> Self {
+        let mut ret = Self::zero();
+        ret.mvec[idx] = f;
+        ret
+    }
+
+    // basis vectors are available as methods
+    pub fn e0() -> Self { R201::new(1.0, 1) }
+    pub fn e1() -> Self { R201::new(1.0, 2) }
+    pub fn e2() -> Self { R201::new(1.0, 3) }
+    pub fn e01() -> Self { R201::new(1.0, 4) }
+    pub fn e02() -> Self { R201::new(1.0, 5) }
+    pub fn e12() -> Self { R201::new(1.0, 6) }
+    pub fn e012() -> Self { R201::new(1.0, 7) }
+}
+
+impl Index<usize> for R201 {
+    type Output = float_t;
+
+    fn index<'a>(&'a self, index: usize) -> &'a Self::Output {
+        &self.mvec[index]
+    }
+}
+
+impl IndexMut<usize> for R201 {
+    fn index_mut<'a>(&'a mut self, index: usize) -> &'a mut Self::Output {
+        &mut self.mvec[index]
+    }
+}
+
+impl fmt::Display for R201 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut n = 0;
+        let ret = self.mvec.iter().enumerate().filter_map(|(i, &coeff)| {
+            if coeff > 0.00001 || coeff < -0.00001 {
+                n = 1;
+                Some(format!("{}{}", 
+                        format!("{:.*}", 7, coeff).trim_end_matches('0').trim_end_matches('.'),
+                        if i > 0 { basis[i] } else { "" }
+                    )
+                )
+            } else {
+                None
+            }
+        }).collect::<Vec<String>>().join(" + ");
+        if n==0 { write!(f,"0") } else { write!(f, "{}", ret) }
+    }
+}
+
+macro_rules! define_binary_op(
+    (
+        // Operator, operator method, and scalar bounds.
+        $Op: ident, $op: ident;
+        // Argument identifiers and types + output.
+        $lhs: ident: $Lhs: ty, $rhs: ident: $Rhs: ty, Output = $Result: ty;
+        // Operator actual implementation.
+        $action: expr;
+        // Lifetime.
+        $($lives: tt),*
+    ) => {
+       impl<$($lives ,)*> $Op<$Rhs> for $Lhs {
+           type Output = $Result;
+
+           #[inline]
+           fn $op($lhs, $rhs: $Rhs) -> Self::Output {
+               $action
+           }
+       }
+    }
+);
+
+macro_rules! define_binary_op_all(
+    (
+        // Operator, operator method, and scalar bounds.
+        $Op: ident, $op: ident;
+        // Argument identifiers and types + output.
+        $lhs: ident: $Lhs: ty, $rhs: ident: $Rhs: ty, Output = $Result: ty;
+        // Operators actual implementations.
+        [val val] => $action_val_val: expr;
+        [ref val] => $action_ref_val: expr;
+        [val ref] => $action_val_ref: expr;
+        [ref ref] => $action_ref_ref: expr;
+    ) => {
+        define_binary_op!(
+            $Op, $op;
+            $lhs: $Lhs, $rhs: $Rhs, Output = $Result;
+            $action_val_val;
+        );
+
+        define_binary_op!(
+            $Op, $op;
+            $lhs: &'a $Lhs, $rhs: $Rhs, Output = $Result;
+            $action_ref_val;
+            'a
+        );
+
+        define_binary_op!(
+            $Op, $op;
+            $lhs: $Lhs, $rhs: &'b $Rhs, Output = $Result;
+            $action_val_ref;
+            'b
+        );
+
+        define_binary_op!(
+            $Op, $op;
+            $lhs: &'a $Lhs, $rhs: &'b $Rhs, Output = $Result;
+            $action_ref_ref;
+            'a, 'b
+        );
+    }
+);
+
+// TODO define_unary_op
+
+
+
+// Reverse
+// Reverse the order of the basis blades.
+impl R201 {
+    pub fn Reverse(self: & Self) -> R201 {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=a[0];
+        res[1]=a[1];
+        res[2]=a[2];
+        res[3]=a[3];
+        res[4]=-a[4];
+        res[5]=-a[5];
+        res[6]=-a[6];
+        res[7]=-a[7];
+        res
+    }
+}
+
+// Dual
+// Poincare duality operator.
+impl R201 {
+    pub fn Dual(self: & Self) -> R201 {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=a[7];
+        res[1]=a[6];
+        res[2]=a[5];
+        res[3]=a[4];
+        res[4]=a[3];
+        res[5]=a[2];
+        res[6]=a[1];
+        res[7]=a[0];
+        res
+    }
+}
+
+impl Not for & R201 {
+    type Output = R201;
+
+    fn not(self: Self) -> R201 {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=a[7];
+        res[1]=a[6];
+        res[2]=a[5];
+        res[3]=a[4];
+        res[4]=a[3];
+        res[5]=a[2];
+        res[6]=a[1];
+        res[7]=a[0];
+        res
+    }
+}
+
+// Conjugate
+// Clifford Conjugation
+impl R201 {
+    pub fn Conjugate(self: & Self) -> R201 {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=a[0];
+        res[1]=-a[1];
+        res[2]=-a[2];
+        res[3]=-a[3];
+        res[4]=-a[4];
+        res[5]=-a[5];
+        res[6]=-a[6];
+        res[7]=a[7];
+        res
+    }
+}
+
+// Involute
+// Main involution
+impl R201 {
+    pub fn Involute(self: & Self) -> R201 {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=a[0];
+        res[1]=-a[1];
+        res[2]=-a[2];
+        res[3]=-a[3];
+        res[4]=a[4];
+        res[5]=a[5];
+        res[6]=a[6];
+        res[7]=-a[7];
+        res
+    }
+}
+
+// Mul
+// The geometric product.
+
+define_binary_op_all!(
+    Mul,
+    mul;
+    self: R201, b: R201, Output = R201;
+    [val val] => &self * &b;
+    [ref val] =>  self * &b;
+    [val ref] => &self *  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=b[0]*a[0]+b[2]*a[2]+b[3]*a[3]-b[6]*a[6];
+		res[1]=b[1]*a[0]+b[0]*a[1]-b[4]*a[2]-b[5]*a[3]+b[2]*a[4]+b[3]*a[5]-b[7]*a[6]-b[6]*a[7];
+		res[2]=b[2]*a[0]+b[0]*a[2]-b[6]*a[3]+b[3]*a[6];
+		res[3]=b[3]*a[0]+b[6]*a[2]+b[0]*a[3]-b[2]*a[6];
+		res[4]=b[4]*a[0]+b[2]*a[1]-b[1]*a[2]+b[7]*a[3]+b[0]*a[4]-b[6]*a[5]+b[5]*a[6]+b[3]*a[7];
+		res[5]=b[5]*a[0]+b[3]*a[1]-b[7]*a[2]-b[1]*a[3]+b[6]*a[4]+b[0]*a[5]-b[4]*a[6]-b[2]*a[7];
+		res[6]=b[6]*a[0]+b[3]*a[2]-b[2]*a[3]+b[0]*a[6];
+		res[7]=b[7]*a[0]+b[6]*a[1]-b[5]*a[2]+b[4]*a[3]+b[3]*a[4]-b[2]*a[5]+b[1]*a[6]+b[0]*a[7];
+        res
+    };
+);
+
+
+// Wedge
+// The outer product. (MEET)
+
+define_binary_op_all!(
+    BitXor,
+    bitxor;
+    self: R201, b: R201, Output = R201;
+    [val val] => &self ^ &b;
+    [ref val] =>  self ^ &b;
+    [val ref] => &self ^  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=b[0]*a[0];
+		res[1]=b[1]*a[0]+b[0]*a[1];
+		res[2]=b[2]*a[0]+b[0]*a[2];
+		res[3]=b[3]*a[0]+b[0]*a[3];
+		res[4]=b[4]*a[0]+b[2]*a[1]-b[1]*a[2]+b[0]*a[4];
+		res[5]=b[5]*a[0]+b[3]*a[1]-b[1]*a[3]+b[0]*a[5];
+		res[6]=b[6]*a[0]+b[3]*a[2]-b[2]*a[3]+b[0]*a[6];
+		res[7]=b[7]*a[0]+b[6]*a[1]-b[5]*a[2]+b[4]*a[3]+b[3]*a[4]-b[2]*a[5]+b[1]*a[6]+b[0]*a[7];
+        res
+    };
+);
+
+
+// Vee
+// The regressive product. (JOIN)
+
+define_binary_op_all!(
+    BitAnd,
+    bitand;
+    self: R201, b: R201, Output = R201;
+    [val val] => &self & &b;
+    [ref val] =>  self & &b;
+    [val ref] => &self &  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[7]=b[7]*a[7];
+		res[6]=b[6]*a[7]+b[7]*a[6];
+		res[5]=b[5]*a[7]+b[7]*a[5];
+		res[4]=b[4]*a[7]+b[7]*a[4];
+		res[3]=b[3]*a[7]+b[5]*a[6]-b[6]*a[5]+b[7]*a[3];
+		res[2]=b[2]*a[7]+b[4]*a[6]-b[6]*a[4]+b[7]*a[2];
+		res[1]=b[1]*a[7]+b[4]*a[5]-b[5]*a[4]+b[7]*a[1];
+		res[0]=b[0]*a[7]+b[1]*a[6]-b[2]*a[5]+b[3]*a[4]+b[4]*a[3]-b[5]*a[2]+b[6]*a[1]+b[7]*a[0];
+        res
+    };
+);
+
+
+// Dot
+// The inner product.
+
+define_binary_op_all!(
+    BitOr,
+    bitor;
+    self: R201, b: R201, Output = R201;
+    [val val] => &self | &b;
+    [ref val] =>  self | &b;
+    [val ref] => &self |  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0]=b[0]*a[0]+b[2]*a[2]+b[3]*a[3]-b[6]*a[6];
+		res[1]=b[1]*a[0]+b[0]*a[1]-b[4]*a[2]-b[5]*a[3]+b[2]*a[4]+b[3]*a[5]-b[7]*a[6]-b[6]*a[7];
+		res[2]=b[2]*a[0]+b[0]*a[2]-b[6]*a[3]+b[3]*a[6];
+		res[3]=b[3]*a[0]+b[6]*a[2]+b[0]*a[3]-b[2]*a[6];
+		res[4]=b[4]*a[0]+b[7]*a[3]+b[0]*a[4]+b[3]*a[7];
+		res[5]=b[5]*a[0]-b[7]*a[2]+b[0]*a[5]-b[2]*a[7];
+		res[6]=b[6]*a[0]+b[0]*a[6];
+		res[7]=b[7]*a[0]+b[0]*a[7];
+        res
+    };
+);
+
+
+// Add
+// Multivector addition
+
+define_binary_op_all!(
+    Add,
+    add;
+    self: R201, b: R201, Output = R201;
+    [val val] => &self + &b;
+    [ref val] =>  self + &b;
+    [val ref] => &self +  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a[0]+b[0];
+		res[1] = a[1]+b[1];
+		res[2] = a[2]+b[2];
+		res[3] = a[3]+b[3];
+		res[4] = a[4]+b[4];
+		res[5] = a[5]+b[5];
+		res[6] = a[6]+b[6];
+		res[7] = a[7]+b[7];
+        res
+    };
+);
+
+
+// Sub
+// Multivector subtraction
+
+define_binary_op_all!(
+    Sub,
+    sub;
+    self: R201, b: R201, Output = R201;
+    [val val] => &self - &b;
+    [ref val] =>  self - &b;
+    [val ref] => &self -  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a[0]-b[0];
+		res[1] = a[1]-b[1];
+		res[2] = a[2]-b[2];
+		res[3] = a[3]-b[3];
+		res[4] = a[4]-b[4];
+		res[5] = a[5]-b[5];
+		res[6] = a[6]-b[6];
+		res[7] = a[7]-b[7];
+        res
+    };
+);
+
+
+// smul
+// scalar/multivector multiplication
+
+define_binary_op_all!(
+    Mul,
+    mul;
+    self: float_t, b: R201, Output = R201;
+    [val val] => &self * &b;
+    [ref val] =>  self * &b;
+    [val ref] => &self *  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a*b[0];
+        res[1] = a*b[1];
+        res[2] = a*b[2];
+        res[3] = a*b[3];
+        res[4] = a*b[4];
+        res[5] = a*b[5];
+        res[6] = a*b[6];
+        res[7] = a*b[7];
+        res
+    };
+);
+
+
+// muls
+// multivector/scalar multiplication
+
+define_binary_op_all!(
+    Mul,
+    mul;
+    self: R201, b: float_t, Output = R201;
+    [val val] => &self * &b;
+    [ref val] =>  self * &b;
+    [val ref] => &self *  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a[0]*b;
+        res[1] = a[1]*b;
+        res[2] = a[2]*b;
+        res[3] = a[3]*b;
+        res[4] = a[4]*b;
+        res[5] = a[5]*b;
+        res[6] = a[6]*b;
+        res[7] = a[7]*b;
+        res
+    };
+);
+
+
+// sadd
+// scalar/multivector addition
+
+define_binary_op_all!(
+    Add,
+    add;
+    self: float_t, b: R201, Output = R201;
+    [val val] => &self + &b;
+    [ref val] =>  self + &b;
+    [val ref] => &self +  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a+b[0];
+        res[1] = b[1];
+        res[2] = b[2];
+        res[3] = b[3];
+        res[4] = b[4];
+        res[5] = b[5];
+        res[6] = b[6];
+        res[7] = b[7];
+        res
+    };
+);
+
+
+// adds
+// multivector/scalar addition
+
+define_binary_op_all!(
+    Add,
+    add;
+    self: R201, b: float_t, Output = R201;
+    [val val] => &self + &b;
+    [ref val] =>  self + &b;
+    [val ref] => &self +  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a[0]+b;
+        res[1] = a[1];
+        res[2] = a[2];
+        res[3] = a[3];
+        res[4] = a[4];
+        res[5] = a[5];
+        res[6] = a[6];
+        res[7] = a[7];
+        res
+    };
+);
+
+
+// ssub
+// scalar/multivector subtraction
+
+define_binary_op_all!(
+    Sub,
+    sub;
+    self: float_t, b: R201, Output = R201;
+    [val val] => &self - &b;
+    [ref val] =>  self - &b;
+    [val ref] => &self -  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a-b[0];
+        res[1] = -b[1];
+        res[2] = -b[2];
+        res[3] = -b[3];
+        res[4] = -b[4];
+        res[5] = -b[5];
+        res[6] = -b[6];
+        res[7] = -b[7];
+        res
+    };
+);
+
+
+// subs
+// multivector/scalar subtraction
+
+define_binary_op_all!(
+    Sub,
+    sub;
+    self: R201, b: float_t, Output = R201;
+    [val val] => &self - &b;
+    [ref val] =>  self - &b;
+    [val ref] => &self -  b;
+    [ref ref] => {
+        let mut res = R201::zero();
+        let a = self;
+        res[0] = a[0]-b;
+        res[1] = a[1];
+        res[2] = a[2];
+        res[3] = a[3];
+        res[4] = a[4];
+        res[5] = a[5];
+        res[6] = a[6];
+        res[7] = a[7];
+        res
+    };
+);
+
+
+impl R201 {
+    pub fn norm(self: & Self) -> float_t {
+        let scalar_part = (self * self.Conjugate())[0];
+
+        scalar_part.abs().sqrt()
+    }
+
+    pub fn inorm(self: & Self) -> float_t {
+        self.Dual().norm()
+    }
+
+    pub fn normalized(self: & Self) -> Self {
+        self * (1.0 / self.norm())
+    }
+    
+    
+
+}
+
+
+fn main() {
+
+  println!("e0*e0         : {}", R201::e0() * R201::e0());
+  println!("pss           : {}", R201::e012());
+  println!("pss*pss       : {}", R201::e012() * R201::e012());
+
+}
